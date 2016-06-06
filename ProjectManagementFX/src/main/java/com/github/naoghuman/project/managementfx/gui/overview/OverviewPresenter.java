452bda7c945ed1796d6191ef0b5ec9f1b3449407
PRo <peter.rogge@yahoo.de>
2016-06-06 05:43:28 +0200

    Basic structure for the view 'Overview'.

---
+++ ProjectManagementFX/src/main/java/com/github/naoghuman/project/managementfx/gui/overview/OverviewPresenter.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2016 Naoghuman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.naoghuman.project.managementfx.gui.overview;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+
+/**
+ *
+ * @author Naoghuman
+ */
+public class OverviewPresenter implements Initializable {
+    
+    @FXML private ListView lvProjectOverview;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+    
+    public void onActionCreateProject() {
+        
+    }
+    
+}
