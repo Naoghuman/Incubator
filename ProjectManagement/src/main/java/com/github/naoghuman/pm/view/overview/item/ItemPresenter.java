cd0de7132d69c72c51ef97c779a1cc62dee84ecf
PRo <peter.rogge@yahoo.de>
2016-06-22 16:43:22 +0200

    Cleanup autor in ItemPresenter.

--- ProjectManagement/src/main/java/com/github/naoghuman/pm/view/overview/item/ItemPresenter.java
+++ ProjectManagement/src/main/java/com/github/naoghuman/pm/view/overview/item/ItemPresenter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 PRo
+ * Copyright (C) 2016 Naoghuman
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -28,7 +28,7 @@ import javafx.scene.layout.Pane;
 
 /**
  *
- * @author PRo
+ * @author Naoghuman
  */
 public class ItemPresenter implements Initializable {
     
--- ProjectManagement/src/main/java/com/github/naoghuman/pm/view/overview/item/ItemView.java
+++ ProjectManagement/src/main/java/com/github/naoghuman/pm/view/overview/item/ItemView.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 PRo
+ * Copyright (C) 2016 Naoghuman
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,7 +20,7 @@ import com.airhacks.afterburner.views.FXMLView;
 
 /**
  *
- * @author PRo
+ * @author Naoghuman
  */
 public class ItemView extends FXMLView {
     
