aa7078ebfdc08a8d035f32aff91ab93d68f743ad
PRo <peter.rogge@yahoo.de>
2016-07-26 19:33:56 +0200

    Add extended resources handling to pom.xml, move SubtlePatternsTileLoader to Lib-Tile-SubtlePatterns-Images.

--- Lib-Tile/Lib-Tile-SubtlePatterns-Images/src/main/java/com/github/naoghuman/lib/tile/subtlepatterns/images/SubtlePatternsTileLoader.java
+++ Lib-Tile/Lib-Tile-SubtlePatterns-Images/src/main/java/com/github/naoghuman/lib/tile/subtlepatterns/images/SubtlePatternsTileLoader.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.github.naoghuman.lib.tile.subtlepatterns;
+package com.github.naoghuman.lib.tile.subtlepatterns.images;
 
 import javafx.scene.image.Image;
 import javafx.scene.layout.Background;
