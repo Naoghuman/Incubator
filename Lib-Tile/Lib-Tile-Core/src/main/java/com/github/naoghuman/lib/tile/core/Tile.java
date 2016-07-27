155605ec56f8757a6d00216e5f4a0e78c007225e
PRo <peter.rogge@yahoo.de>
2016-07-27 21:40:48 +0200

    Add JavaDoc to the interface Tile.

--- Lib-Tile/Lib-Tile-Core/src/main/java/com/github/naoghuman/lib/tile/core/Tile.java
+++ Lib-Tile/Lib-Tile-Core/src/main/java/com/github/naoghuman/lib/tile/core/Tile.java
@@ -17,44 +17,62 @@
 package com.github.naoghuman.lib.tile.core;
 
 /**
+ * A <code>Tile</code> is per definition a little transparent image which overlay
+ * a background color or image with the help of repetitions from the image.
  *
  * @author Naoghuman
  */
 public interface Tile {
 
     /**
+     * The autor is the creator from the <code>Tile</code>. This parameter is 
+     * optional. If this parameter is NULL or empty then the parameter 
+     * <code>autorUrl</code> won't be evaluated.
      * 
-     * @return 
+     * @return the autor from the <code>Tile</code>.
+     * @see #getAutorUrl()
      */
     public String getAutor();
     
     /**
+     * The autor url from the <code>Tile</code>. This parameter is optional. If 
+     * the parameter <code>autor</code> is NULL or empty then this parameter 
+     * won't be evaluated.
      * 
-     * @return 
+     * @return the autor url from the <code>Tile</code>.
+     * @see #getAutor()
      */
     public String getAutorUrl();
     
     /**
+     * The height from a single <code>Tile</code> image. This parameter can't 
+     * be <= 0.0d.
      * 
-     * @return 
+     * @return the height from a single <code>Tile</code> image.
      */
     public double getHeight();
     
     /**
+     * The image name from the <code>Tile</code>. This parameter can't be NULL 
+     * or empty.
      * 
-     * @return 
+     * @return the image name from the <code>Tile</code>.
      */
     public String getImageName();
     
     /**
+     * The title from the <code>Tile</code>. This parameter can't be NULL or 
+     * empty.
      * 
-     * @return 
+     * @return the title from the <code>Tile</code>.
      */
     public String getTitle();
     
     /**
+     * The width from a single <code>Tile</code> image. This parameter can't 
+     * be <= 0.0d.
      * 
-     * @return 
+     * @return the width from a single <code>Tile</code> image.
      */
     public double getWidth();
 
