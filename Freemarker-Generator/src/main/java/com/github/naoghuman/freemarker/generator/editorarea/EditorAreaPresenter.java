a907a27fe9aa5c03cdaced6affe338ac257d9c23
Peter Rogge <peter.rogge@yahoo.de>
2016-09-21 20:49:59 +0200

    Basic layout for the 'EditorArea'.

---
+++ Freemarker-Generator/src/main/java/com/github/naoghuman/freemarker/generator/editorarea/EditorAreaPresenter.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2016 Naoghuman
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.github.naoghuman.freemarker.generator.editorarea;
+
+import com.github.naoghuman.lib.logger.api.LoggerFacade;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author Naoghuman
+ */
+public class EditorAreaPresenter implements Initializable {
+
+    @FXML private AnchorPane apTemplateArea;
+    
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        LoggerFacade.getDefault().info(this.getClass(), "Initialize EditorAreaPresenter"); // NOI18N
+        
+    }
+    
+}
